68ea2fde51d80fbaff2146596cfcc5b165e14c75
Chad Collins <chadrcollins91@gmail.com>
2024-02-28 06:56:11 -0500

    copy now returning new address

--- src/data/copy.rs
+++ src/data/copy.rs
@@ -4,13 +4,28 @@ pub fn copy_data_at_to_data<Data: GarnishData>(
     data_addr: Data::Size,
     from: &Data,
     to: &mut Data,
-) -> Result<(), Data::Error> {
-    match from.get_data_type(data_addr)? {
+) -> Result<Data::Size, Data::Error> {
+    Ok(match from.get_data_type(data_addr)? {
+        GarnishDataType::Invalid => todo!(),
+        GarnishDataType::Unit => todo!(),
         GarnishDataType::Number => to.add_number(from.get_number(data_addr)?)?,
-        _ => todo!(),
-    };
-
-    Ok(())
+        GarnishDataType::Type => todo!(),
+        GarnishDataType::Char => todo!(),
+        GarnishDataType::CharList => todo!(),
+        GarnishDataType::Byte => todo!(),
+        GarnishDataType::ByteList => todo!(),
+        GarnishDataType::Symbol => todo!(),
+        GarnishDataType::Pair => todo!(),
+        GarnishDataType::Range => todo!(),
+        GarnishDataType::Concatenation => todo!(),
+        GarnishDataType::Slice => todo!(),
+        GarnishDataType::List => todo!(),
+        GarnishDataType::Expression => todo!(),
+        GarnishDataType::External => todo!(),
+        GarnishDataType::True => todo!(),
+        GarnishDataType::False => todo!(),
+        GarnishDataType::Custom => todo!(),
+    })
 }
 
 #[cfg(test)]
@@ -29,8 +44,9 @@ mod tests {
         to.add_number(SimpleNumber::Integer(20)).unwrap();
         to.add_number(SimpleNumber::Integer(30)).unwrap();
 
-        copy_data_at_to_data(addr, &from, &mut to).unwrap();
+        let new_addr = copy_data_at_to_data(addr, &from, &mut to).unwrap();
 
+        assert_eq!(new_addr, 6);
         assert_eq!(
             to.get_data().get(6).unwrap().as_number().unwrap(),
             SimpleNumber::Integer(40)
