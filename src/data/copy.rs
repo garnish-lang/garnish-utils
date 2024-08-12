9329bd807c85a21d932876d07d9434a8450bb1d1
chadrc <chadrcollins91@gmail.com>
2024-08-12 10:09:48 -0400

    Support for handlers to clone custom and invalid data

--- src/data/copy.rs
+++ src/data/copy.rs
@@ -7,10 +7,11 @@ pub fn clone_data<Data: GarnishData>(
     from: &Data,
     to: &mut Data,
 ) -> Result<Data::Size, Data::Error> {
-    clone_data_with_handlers(
+    clone_data_with_handlers_internal(
         data_addr,
         from,
         to,
+        None,
         None,
     )
 }
@@ -21,11 +22,27 @@ pub fn clone_data_with_custom_handler<Data: GarnishData>(
     to: &mut Data,
     custom_handler: CloneHandler<Data>,
 ) -> Result<Data::Size, Data::Error> {
-    clone_data_with_handlers(
+    clone_data_with_handlers_internal(
         data_addr,
         from,
         to,
         Some(custom_handler),
+        None
+    )
+}
+
+pub fn clone_data_with_invalid_handler<Data: GarnishData>(
+    data_addr: Data::Size,
+    from: &Data,
+    to: &mut Data,
+    invalid_handler: CloneHandler<Data>,
+) -> Result<Data::Size, Data::Error> {
+    clone_data_with_handlers_internal(
+        data_addr,
+        from,
+        to,
+        None,
+        Some(invalid_handler),
     )
 }
 
@@ -33,11 +50,30 @@ pub fn clone_data_with_handlers<Data: GarnishData>(
     data_addr: Data::Size,
     from: &Data,
     to: &mut Data,
+    custom_handler: CloneHandler<Data>,
+    invalid_handler: CloneHandler<Data>, // to be implemented
+) -> Result<Data::Size, Data::Error> {
+    clone_data_with_handlers_internal(
+        data_addr,
+        from,
+        to,
+        Some(custom_handler),
+        Some(invalid_handler),
+    )
+}
+
+fn clone_data_with_handlers_internal<Data: GarnishData>(
+    data_addr: Data::Size,
+    from: &Data,
+    to: &mut Data,
     custom_handler: Option<CloneHandler<Data>>,
-    // invalid_handler: Option<CloneHandler<Data>>, // to be implemented
+    invalid_handler: Option<CloneHandler<Data>>, // to be implemented
 ) -> Result<Data::Size, Data::Error> {
     match from.get_data_type(data_addr.clone())? {
-        GarnishDataType::Invalid => to.add_unit(), // will add custom handler
+        GarnishDataType::Invalid => match invalid_handler {
+            None => to.add_unit(),
+            Some(handler) => handler(data_addr.clone(), from, to)
+        }
         GarnishDataType::Custom => match custom_handler {
             None => to.add_unit(),
             Some(handler) => handler(data_addr.clone(), from, to)
@@ -125,9 +161,10 @@ pub fn clone_data_with_handlers<Data: GarnishData>(
 
 #[cfg(test)]
 mod tests {
-    use crate::data::{clone_data, clone_data_with_custom_handler};
+    use crate::data::{clone_data, clone_data_with_custom_handler, clone_data_with_handlers, clone_data_with_invalid_handler};
     use garnish_lang_simple_data::{SimpleGarnishData, SimpleNumber};
     use garnish_lang_traits::{GarnishData, GarnishDataType};
+    use crate::data::copy::test_data_impl::TestData;
 
     #[test]
     fn copy_number() {
@@ -402,6 +439,60 @@ mod tests {
     }
 
     #[test]
+    fn copy_invalid_no_handler() {
+        let from = TestData::new();
+        let mut to = TestData::new();
+
+        clone_data(0, &from, &mut to).unwrap();
+
+        assert!(to.unit_added);
+        assert!(!to.number_added);
+    }
+
+    #[test]
+    fn copy_invalid_handler() {
+        let from = TestData::new();
+        let mut to = TestData::new();
+
+        clone_data_with_invalid_handler(
+            0, &from, &mut to,
+            | _, _, to| {
+            to.add_number(SimpleNumber::Integer(10))
+        }).unwrap();
+
+        assert!(!to.unit_added);
+        assert!(to.number_added);
+    }
+
+    #[test]
+    fn copy_with_handlers() {
+        let from = TestData::new();
+        let mut to = TestData::new();
+
+        clone_data_with_handlers(
+            0, &from, &mut to,
+            | _, _, to| {
+                to.add_number(SimpleNumber::Integer(10))
+            }, |_, _, to| {
+                to.add_char(0)
+            }).unwrap();
+
+        assert!(to.char_added);
+
+        clone_data_with_handlers(
+            1, &from, &mut to,
+            | _, _, to| {
+                to.add_number(SimpleNumber::Integer(10))
+            }, |_, _, to| {
+                to.add_char(0)
+            }).unwrap();
+
+        assert!(to.number_added);
+
+        assert!(!to.unit_added);
+    }
+
+    #[test]
     fn copy_pair() {
         let mut from = SimpleGarnishData::new();
         let d1 = from.add_symbol(100).unwrap();
@@ -578,5 +669,446 @@ mod tests {
             to.get_data().get(8).unwrap().as_number().unwrap(),
             SimpleNumber::Integer(300)
         );
+    }
+}
+
+#[cfg(test)]
+#[allow(unused)]
+mod test_data_impl {
+    use std::collections::hash_map::Iter;
+    use garnish_lang_simple_data::{DataError, NumberIterator, SimpleGarnishData, SimpleNumber, SizeIterator};
+    use garnish_lang_traits::{GarnishData, GarnishDataType, Instruction};
+
+    pub struct TestData {
+        pub unit_added: bool,
+        pub number_added: bool,
+        pub char_added: bool
+    }
+
+    impl TestData {
+        pub fn new() -> Self {
+            Self {
+                unit_added: false,
+                number_added: false,
+                char_added: false
+            }
+        }
+    }
+
+    impl GarnishData for TestData {
+        type Error = DataError;
+        type Symbol = usize;
+        type Byte = usize;
+        type Char = usize;
+        type Number = SimpleNumber;
+        type Size = usize;
+        type SizeIterator = SizeIterator;
+        type NumberIterator = NumberIterator;
+        type InstructionIterator = SizeIterator;
+        type DataIndexIterator = SizeIterator;
+        type ValueIndexIterator = SizeIterator;
+        type RegisterIndexIterator = SizeIterator;
+        type JumpTableIndexIterator = SizeIterator;
+        type JumpPathIndexIterator = SizeIterator;
+        type ListIndexIterator = NumberIterator;
+
+        fn get_data_len(&self) -> Self::Size {
+            unimplemented!()
+        }
+
+        fn get_data_iter(&self) -> Self::DataIndexIterator {
+            unimplemented!()
+        }
+
+        fn get_value_stack_len(&self) -> Self::Size {
+            unimplemented!()
+        }
+
+        fn push_value_stack(&mut self, addr: Self::Size) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn pop_value_stack(&mut self) -> Option<Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_value(&self, addr: Self::Size) -> Option<Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_value_mut(&mut self, addr: Self::Size) -> Option<&mut Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_current_value(&self) -> Option<Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_current_value_mut(&mut self) -> Option<&mut Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_value_iter(&self) -> Self::ValueIndexIterator {
+            unimplemented!()
+        }
+
+        fn get_data_type(&self, addr: Self::Size) -> Result<GarnishDataType, Self::Error> {
+            if addr == 1 {
+                Ok(GarnishDataType::Custom)
+            } else {
+                Ok(GarnishDataType::Invalid)
+            }
+        }
+
+        fn get_number(&self, addr: Self::Size) -> Result<Self::Number, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_type(&self, addr: Self::Size) -> Result<GarnishDataType, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_char(&self, addr: Self::Size) -> Result<Self::Char, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_byte(&self, addr: Self::Size) -> Result<Self::Byte, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_symbol(&self, addr: Self::Size) -> Result<Self::Symbol, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_expression(&self, addr: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_external(&self, addr: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_pair(&self, addr: Self::Size) -> Result<(Self::Size, Self::Size), Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_concatenation(&self, addr: Self::Size) -> Result<(Self::Size, Self::Size), Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_range(&self, addr: Self::Size) -> Result<(Self::Size, Self::Size), Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_slice(&self, addr: Self::Size) -> Result<(Self::Size, Self::Size), Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_list_len(&self, addr: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_list_item(&self, list_addr: Self::Size, item_addr: Self::Number) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_list_associations_len(&self, addr: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_list_association(&self, list_addr: Self::Size, item_addr: Self::Number) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_list_item_with_symbol(&self, list_addr: Self::Size, sym: Self::Symbol) -> Result<Option<Self::Size>, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_list_items_iter(&self, list_addr: Self::Size) -> Self::ListIndexIterator {
+            unimplemented!()
+        }
+
+        fn get_list_associations_iter(&self, list_addr: Self::Size) -> Self::ListIndexIterator {
+            unimplemented!()
+        }
+
+        fn get_char_list_len(&self, addr: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_char_list_item(&self, addr: Self::Size, item_index: Self::Number) -> Result<Self::Char, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_char_list_iter(&self, list_addr: Self::Size) -> Self::ListIndexIterator {
+            unimplemented!()
+        }
+
+        fn get_byte_list_len(&self, addr: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_byte_list_item(&self, addr: Self::Size, item_index: Self::Number) -> Result<Self::Byte, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_byte_list_iter(&self, list_addr: Self::Size) -> Self::ListIndexIterator {
+            unimplemented!()
+        }
+
+        fn add_unit(&mut self) -> Result<Self::Size, Self::Error> {
+            self.unit_added = true;
+            Ok(0)
+        }
+
+        fn add_true(&mut self) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_false(&mut self) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_number(&mut self, value: Self::Number) -> Result<Self::Size, Self::Error> {
+            self.number_added = true;
+            Ok(0)
+        }
+
+        fn add_type(&mut self, value: GarnishDataType) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_char(&mut self, value: Self::Char) -> Result<Self::Size, Self::Error> {
+            self.char_added = true;
+            Ok(0)
+        }
+
+        fn add_byte(&mut self, value: Self::Byte) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_symbol(&mut self, value: Self::Symbol) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_expression(&mut self, value: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_external(&mut self, value: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_pair(&mut self, value: (Self::Size, Self::Size)) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_concatenation(&mut self, left: Self::Size, right: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_range(&mut self, start: Self::Size, end: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_slice(&mut self, list: Self::Size, range: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn start_list(&mut self, len: Self::Size) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_to_list(&mut self, addr: Self::Size, is_associative: bool) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn end_list(&mut self) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn start_char_list(&mut self) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_to_char_list(&mut self, c: Self::Char) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn end_char_list(&mut self) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn start_byte_list(&mut self) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_to_byte_list(&mut self, c: Self::Byte) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn end_byte_list(&mut self) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_register_len(&self) -> Self::Size {
+            unimplemented!()
+        }
+
+        fn push_register(&mut self, addr: Self::Size) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_register(&self, addr: Self::Size) -> Option<Self::Size> {
+            unimplemented!()
+        }
+
+        fn pop_register(&mut self) -> Result<Option<Self::Size>, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_register_iter(&self) -> Self::RegisterIndexIterator {
+            unimplemented!()
+        }
+
+        fn get_instruction_len(&self) -> Self::Size {
+            unimplemented!()
+        }
+
+        fn push_instruction(&mut self, instruction: Instruction, data: Option<Self::Size>) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_instruction(&self, addr: Self::Size) -> Option<(Instruction, Option<Self::Size>)> {
+            unimplemented!()
+        }
+
+        fn get_instruction_iter(&self) -> Self::InstructionIterator {
+            unimplemented!()
+        }
+
+        fn get_instruction_cursor(&self) -> Self::Size {
+            unimplemented!()
+        }
+
+        fn set_instruction_cursor(&mut self, addr: Self::Size) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_jump_table_len(&self) -> Self::Size {
+            unimplemented!()
+        }
+
+        fn push_jump_point(&mut self, index: Self::Size) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn get_jump_point(&self, index: Self::Size) -> Option<Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_jump_point_mut(&mut self, index: Self::Size) -> Option<&mut Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_jump_table_iter(&self) -> Self::JumpTableIndexIterator {
+            unimplemented!()
+        }
+
+        fn push_jump_path(&mut self, index: Self::Size) -> Result<(), Self::Error> {
+            unimplemented!()
+        }
+
+        fn pop_jump_path(&mut self) -> Option<Self::Size> {
+            unimplemented!()
+        }
+
+        fn get_jump_path_iter(&self) -> Self::JumpPathIndexIterator {
+            unimplemented!()
+        }
+
+        fn size_to_number(from: Self::Size) -> Self::Number {
+            unimplemented!()
+        }
+
+        fn number_to_size(from: Self::Number) -> Option<Self::Size> {
+            unimplemented!()
+        }
+
+        fn number_to_char(from: Self::Number) -> Option<Self::Char> {
+            unimplemented!()
+        }
+
+        fn number_to_byte(from: Self::Number) -> Option<Self::Byte> {
+            unimplemented!()
+        }
+
+        fn char_to_number(from: Self::Char) -> Option<Self::Number> {
+            unimplemented!()
+        }
+
+        fn char_to_byte(from: Self::Char) -> Option<Self::Byte> {
+            unimplemented!()
+        }
+
+        fn byte_to_number(from: Self::Byte) -> Option<Self::Number> {
+            unimplemented!()
+        }
+
+        fn byte_to_char(from: Self::Byte) -> Option<Self::Char> {
+            unimplemented!()
+        }
+
+        fn add_char_list_from(&mut self, from: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_byte_list_from(&mut self, from: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_symbol_from(&mut self, from: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_byte_from(&mut self, from: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn add_number_from(&mut self, from: Self::Size) -> Result<Self::Size, Self::Error> {
+            unimplemented!()
+        }
+
+        fn parse_number(from: &str) -> Result<Self::Number, Self::Error> {
+            unimplemented!()
+        }
+
+        fn parse_symbol(from: &str) -> Result<Self::Symbol, Self::Error> {
+            unimplemented!()
+        }
+
+        fn parse_char(from: &str) -> Result<Self::Char, Self::Error> {
+            unimplemented!()
+        }
+
+        fn parse_byte(from: &str) -> Result<Self::Byte, Self::Error> {
+            unimplemented!()
+        }
+
+        fn parse_char_list(from: &str) -> Result<Vec<Self::Char>, Self::Error> {
+            unimplemented!()
+        }
+
+        fn parse_byte_list(from: &str) -> Result<Vec<Self::Byte>, Self::Error> {
+            unimplemented!()
+        }
+
+        fn make_size_iterator_range(min: Self::Size, max: Self::Size) -> Self::SizeIterator {
+            unimplemented!()
+        }
+
+        fn make_number_iterator_range(min: Self::Number, max: Self::Number) -> Self::NumberIterator {
+            unimplemented!()
+        }
     }
 }
